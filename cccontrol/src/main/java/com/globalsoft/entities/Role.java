ee7f0b3bb51c0f307a0b8bda39bf2bff3d3e30b9
Oromar Lamounier Dantas Melo <oldm@cin.ufpe.br>
2015-12-03 15:49:30 -0300

    Adding Role class

---
+++ cccontrol/src/main/java/com/globalsoft/entities/Role.java
@@ -0,0 +1,23 @@
+package com.globalsoft.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role extends BasicEntity {
+
+	private static final long serialVersionUID = -1385928286137454444L;
+
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Boolean validate() throws Exception {
+		return null;
+	}
+}
